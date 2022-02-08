c3f3b6aa218b0518464f42a637b9231fa04cf2b9
Kennedy Soares <kennedybarros413@gmail.com>
2022-02-08 10:55:14 -0400

    add scroll reveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 const navMenu = document.getElementById('nav-menu'),
   navToggle = document.getElementById('nav-toggle'),
   navClose = document.getElementById('nav-close');
+
 if (navToggle) {
   navToggle.addEventListener('click', () => {
     navMenu.classList.add('show-menu');
@@ -13,40 +14,42 @@ if (navClose) {
   });
 }
 
-const navLink = document.querySelectorAll('.nav__link');
+const navLink = document.querySelectorAll('.nav_link');
+
 function linkAction() {
   const navMenu = document.getElementById('nav-menu');
   navMenu.classList.remove('show-menu');
 }
 navLink.forEach((n) => n.addEventListener('click', linkAction));
-function scrollHeader() {
-  const header = document.getElementById('header');
-  if (this.scrollY >= 50) header.classList.add('scroll-header');
-  else header.classList.remove('scroll-header');
-}
-window.addEventListener('scroll', scrollHeader);
 
 const themeButton = document.getElementById('theme-button');
 const darkTheme = 'dark-theme';
-const iconTheme = 'bx-sun';
+const iconTheme = 'dark-change-theme';
 const selectedTheme = localStorage.getItem('selected-theme');
 const selectedIcon = localStorage.getItem('selected-icon');
 const getCurrentTheme = () =>
   document.body.classList.contains(darkTheme) ? 'dark' : 'light';
-const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun';
+
 if (selectedTheme) {
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
     darkTheme
   );
-  themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](
+  themeButton.classList[selectedIcon === 'change-theme' ? 'add' : 'remove'](
     iconTheme
   );
 }
-
 themeButton.addEventListener('click', () => {
   document.body.classList.toggle(darkTheme);
   themeButton.classList.toggle(iconTheme);
   localStorage.setItem('selected-theme', getCurrentTheme());
-  localStorage.setItem('selected-icon', getCurrentIcon());
 });
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+});
+
+sr.reveal(`.home_img`, { delay: 600 });
+sr.reveal(`.home_data`, { delay: 300 });
