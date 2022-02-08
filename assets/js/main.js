b2462ac9f3c37b10138758a8ae954c3090e75978
Kennedy Soares <kennedybarros413@gmail.com>
2022-02-08 19:11:47 -0400

    add about-me

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,5 +51,5 @@ const sr = ScrollReveal({
   delay: 400,
 });
 
-sr.reveal(`.home_img`, { delay: 600 });
-sr.reveal(`.home_data`, { delay: 300 });
+sr.reveal(`.home_img, .about_img`, { delay: 600 });
+sr.reveal(`.home_data, .about_emp, .about_text`, { delay: 300 });
