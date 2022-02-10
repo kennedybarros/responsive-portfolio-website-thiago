600c859c11a3a37c11382a7016cd89f240a9b7c5
Kennedy Soares <kennedybarros413@gmail.com>
2022-02-10 11:32:55 -0400

    add scroll up

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,3 +79,10 @@ function scrollActive() {
   });
 }
 window.addEventListener('scroll', scrollActive);
+
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up');
+  if (this.scrollY >= 350) scrollUp.classList.add('show-scroll');
+  else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
